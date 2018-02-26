ad51c93ec33c3315891a47bac6970b7af5896c98
caitlinkuhlman <caitlinkuhlman@gmail.com>
2018-02-26 17:11:51 -0500

    agenda

--- nav_color.js
+++ nav_color.js
@@ -1,8 +1,9 @@
 var top1 = 0;
 var top2 = $('#about').offset().top;
 var top3 = $('#program').offset().top;
-var top4 = $('#contact').offset().top;
-var top5 = $('#team').offset().top;
+var top4 = $('#speakers').offset().top - 5;
+var top5 = $('#contact').offset().top;
+var top6 = $('#team').offset().top;
 
 $(document).scroll(function() {
   var scrollPos = $(document).scrollTop();
@@ -15,10 +16,13 @@ $(document).scroll(function() {
   } else if (scrollPos >= top3 && scrollPos < top4) {
     $('#mainNav .nav-link').css('color', 'white');
     $('#mainNav a:hover').css('color', '#212529');
-  } else if (scrollPos >= top4 && scrollPos < top5) {
+  } else if (scrollPos >= top3 && scrollPos < top5) {
+    $('#mainNav .nav-link').css('color', 'white');
+    $('#mainNav a:hover').css('color', '#212529');
+  }else if (scrollPos >= top4 && scrollPos < top6) {
     $('#mainNav .nav-link').css('color', '#212529');
     $('#mainNav a:hover').css('color', '#b30000');
-  } else if (scrollPos >= top5) {
+  } else if (scrollPos >= top6) {
     $('#mainNav .nav-link').css('color', 'white');
     $('#mainNav a:hover').css('color', '#b30000');
   }
